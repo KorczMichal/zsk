196638487aba3e7cc64ebced5d8337d084ed4eaf
KorczMichal <korczmichal09@gmail.com>
2019-09-17 10:21:14 +0200

    12314

---
+++ trzy.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head></head>
+    <body>
+    <?php
+        $liczba = 10;
+        $_liczba = 10;
+        $imię = ='Janusz';
+        $prawda=true;
+
+        $calkowita = 10; // typ int
+        $he = 0xA;
+        echo $hex;
+        ?>
+    </body>
+</html>
