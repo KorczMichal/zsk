d36a9b821ad2e1fb3bb862f47b39e9f9b5ebc406
KorczMichal <korczmichal09@gmail.com>
2019-09-24 11:24:16 +0200

    jhfh

---
+++ dołączaniedopliku.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+
+    </head>
+<body>
+    Plik główny<br>
+    <?php
+    /*include('skrypt.php');
+    include_once('skrypt.php');
+    */
+    require('skrypt.php');
+    require_once('skrypt.php');
+    ?>
+    Dalsza część pliku głównego<br>
+
+    </body>
+</html>
---
+++ skrypt.php
@@ -0,0 +1,4 @@
+<?php
+    echo "Zawartość pliku skrypt.php<br>"
+
+    ?>
