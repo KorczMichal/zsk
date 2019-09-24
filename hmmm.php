53047866d9e7695c04cc06e0677c09d8eaeea50f
KorczMichal <korczmichal09@gmail.com>
2019-09-24 10:49:54 +0200

    nowe

---
+++ hmmm.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+
+<?php
+    $x=2;
+echo $x++;//2
+echo ++$x;//4
+echo $x;//4
+$y =  $x;
+echo $y;//4
+$y = ++$x;
+echo $y;//5
+echo ++$y;//6
+;
+?>
+</body>
+</html>
