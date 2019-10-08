d37e3eb84a9b42702127318f67ff7119647bdee2
KorczMichal <korczmichal09@gmail.com>
2019-10-08 11:23:58 +0200

    123

---
+++ 5_zadanko.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<!--
+Użytkownik podaje dane z klawiatury w polach textarea, text iraz number
+Wyczyść dane z białych znaków.
+Ogranicz pole text area do 50 znaków.
+wyświetl dane z text area zwracając uwagę na znaki break
+Formmularz dodaj z drugiego pliku o nazwie form.php
+?-->
+</body>
+</html>
